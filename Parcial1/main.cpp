049dac6665e781c409400ba4d0a9261e1fac28b4
gio0070 <70187142+gio0070@users.noreply.github.com>
2020-10-17 15:10:00 -0500

    Definicion de las variables para resolver el problema

---
+++ Parcial1/main.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <math.h>
+#include <time.h>
+#include <stdlib.h>
+
+using namespace std;
+
+int main()
+{
+    int d, Ho, Hd, alfao, alfad, opc;
+    int Xo, Yo, Xd, Yd;
+
+    cout<<"Sistema de simulación."<<endl;
+    cout<<"1. Generar disparos (al menos tres) ofensivos que comprometan la integridad del cañón defensivo."<<endl;
+    cout<<"2. Generar disparos (al menos tres) defensivos que comprometan la integridad del cañón ofensivo."<<endl;
+    cout<<"3. Dado un disparo ofensivo, generar (al menos tres) disparos defensivos que impida que el cañón defensivo sea destruido sin importar si el cañón ofensivo pueda ser destruido."<<endl;
+    cout<<"4. Dado un disparo ofensivo, generar (al emnos tres) disparo defensivos que impidan que los cañones defensivo y ofensivo puedan ser destruidos"<<endl;
+
+
+
+    return 0;
+}
